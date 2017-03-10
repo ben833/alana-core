a5aba44e32e1bc26341a58f2de41e069f886d218
Adam Juhasz <adam@akitram.com>
2017-03-10 18:20:58 -0500

    better testing with bluebird config

--- test/_setup.js
+++ test/_setup.js
@@ -1,3 +1,15 @@
+const Promise = require('bluebird');
 before(function(){
   require('source-map-support').install();
+  Promise.config({
+    // Enable long stack traces
+    longStackTraces: true,
+    // Enable cancellation
+    cancellation: true,
+    // // Enable monitoring
+    // monitoring: true,
+    warnings: {
+      wForgottenReturn: false
+    }
+  });
 })
