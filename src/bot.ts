440bb64819ce234976d78258a8cec525e0e729d6
Adam Juhasz <adam@akitram.com>
2017-03-22 14:12:28 -0400

    push greeting fron bot to platform

--- src/bot.ts
+++ src/bot.ts
@@ -151,18 +151,7 @@ export default class Alana {
     let request: Incoming = null;
     let response: Outgoing = null;
     return this.userMiddleware.getUser(basicUser)
-      .catch((err: Error) => {
-        const newUser = _.merge(this.createEmptyUser(), basicUser);
-        if (message.type === 'greeting') {
-          return this.userMiddleware.saveUser(newUser);
-        }
-        const greeting: GreetingMessage = {
-          type: 'greeting',
-        };
-        return this.userMiddleware.saveUser(newUser)
-          .then(() => this._processMessage(basicUser, greeting))
-          .then(() => newUser);
-      })
+      .catch((err: Error) => _.merge(this.createEmptyUser(), basicUser))
       .then(completeUser => {
         completeUser._platform = basicUser._platform;
         completeUser.conversation = completeUser.conversation.concat(message);
